c2d3b650297860d4739d5345896c81c4acbe1cd4
goulce01 <goulce01@luther.edu>
2015-10-21 13:16:19 -0500

    Update interactivetodo.js

--- interactivetodo.js
+++ interactivetodo.js
@@ -1,1 +1,9 @@
-i
+<script type= "type/javascript">
+Newtask = function() {
+var myitem;
+var mylist;
+ myitem = Number.parseInt(document.querySelector("#listitem")).value;
+ document.querySelector("#tasklist").innerHTML = list;
+ newitem = document.createElement("li");
+ tasklist = document.createElement("ul");
+}
