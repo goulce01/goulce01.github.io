b9bd77d75055d273e804aa13d075aaac8264f543
goulce01 <goulce01@luther.edu>
2015-10-22 11:14:26 -0500

    Update interactivetodo.js

--- interactivetodo.js
+++ interactivetodo.js
@@ -1,8 +1,20 @@
 <script type= "type/javascript">
 Newtask = function() {
 var myItem;
+
 var myList;
+
+var myPriority;
+
+var myTaskin;
+
  myList = document.querySelector("#tasklist");
  myItem = document.createElement("li");
+ myPriority = document.querySelector("#priority");
+ myTaskin = document.querySelector("#taskin");
+ myTaskin.type = "checkbox";
+ myList.appendChild(myItem);
+ myItem.classList.add(myPriority.value);
+ myItem.appendChild(myTaskin);
  myItem.innerHTML = document.querySelector("#taskin").value;
  
